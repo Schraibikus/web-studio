bfa1fb8051821bc9c6c041ffadad927d747ffa62
Roman Ulitin <kurt-77@mail.ru>
2024-08-06 20:50:30 +0500

    fix 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout/layout";
+import Link from "next/link";
 
 export default function PageNotFound() {
   return (
@@ -6,9 +7,9 @@ export default function PageNotFound() {
       <div>
         <h1>К сожалению, такой страницы не существует</h1>
         <div>
-          Вы можете вернуться на <a href="/">главную страницу</a>, либо оставить
-          свой комментарий на
-          <a href="/contacts"> странице контактов</a>.
+          Вы можете вернуться на <Link href="/">главную страницу</Link>, либо
+          оставить свой комментарий на
+          <Link href="/contacts"> странице контактов</Link>.
         </div>
       </div>
     </Layout>
